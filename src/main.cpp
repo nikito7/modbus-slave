022f6761cd34fc9831fe3d921770af663ec310c1
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-03-07 00:19:44 +0000

    alpha

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+#include <ModbusRTUSlave.h>
+
+uint16_t inputRegisters[211];
+
+ModbusRTUSlave modbus(Serial, 255);
+
+void setup() {
+  //
+  modbus.configureInputRegisters(inputRegisters, 210);
+  modbus.begin(1, 9600);
+
+  for (uint8_t i = 0; i < sizeof(inputRegisters); i++) {
+    inputRegisters[i] = i;
+  }
+
+}  // eof setup
+
+void loop() { modbus.poll(); }
+
+// EOF
